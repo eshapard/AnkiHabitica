b0d5b75543a54c053f4f649e215ee0e34af94eb1
eshapard <ed.shapard@gmail.com>
2016-06-19 11:51:14 -0700

    Import all .py files in dir
    import all .py files in the directory so we can simply import the entire directory from the main file.

--- AnkiHabitica_v2.0/AnkiHabitica/__init__.py
+++ AnkiHabitica_v2.0/AnkiHabitica/__init__.py
@@ -1,1 +1,7 @@
 #Blank File, allows including this subdirectory
+import os
+for module in os.listdir(os.path.dirname(__file__)):
+    if module == '__init__.py' or module[-3:] != '.py':
+        continue
+    __import__(module[:-3], locals(), globals())
+del module
