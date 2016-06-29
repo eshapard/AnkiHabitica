58546eda81c2a5b3eb6bf1cf08e2c61d7f13a930
eshapard <ed.shapard@gmail.com>
2016-06-28 18:06:15 -0700

    Add files via upload

---
+++ daily_habit.py
@@ -0,0 +1,46 @@
+#daily habit support for Anki Habitica
+import datetime
+import time
+import thread
+from ah_common import AnkiHabiticaCommon as ah
+from anki.hooks import addHook
+import db_helper
+
+
+#-------------Configuration Section------------------------#
+
+habitName = "Anki:mahjong:" #Name of daily habit to score
+minTime = 15 #Minimum amount of time required in minutes
+
+#-------------Do not edit below----------------------------#
+habitID = None #empty placeholder
+minTime = minTime * 60 #set minTime to seconds
+
+#set up initial times
+midnight = datetime.datetime.combine(datetime.date.today(), datetime.time.min)
+epoch = datetime.datetime.utcfromtimestamp(0)
+checkTime = int((midnight - epoch).total_seconds())
+del midnight
+del epoch
+
+#Mark daily as complete
+def mark_daily_complete():
+    global checkTime, habitID
+    if not habitID:
+        habitID = ah.habitica.api.find_habit_id(habitName)
+    if habitID:
+        if ah.habitica.api.perform_task(habitID, "up"):
+            checkTime = checkTime + (24 * 60 * 60)
+
+#Check to see if we have met minimum time requirements
+def check_for_min_time():
+    #return if checkTime is scheduled for later
+    if int(time.time()) < checkTime:
+        return
+    timeToday = db_helper.seconds_count(checkTime)
+    if timeToday >= minTime:
+        #Mark daily habit complete if not already
+        thread.start_new_thread(mark_daily_complete, ())
+
+
+addHook("HabiticaAfterScore", check_for_min_time)
