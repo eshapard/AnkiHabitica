a28e3557c5309362203b78847633ffd4ef4a3d84
Tim Wilson <twilsonco@gmail.com>
2016-07-03 23:17:04 -0600

    Added a progress bar to the backlog scoreing utility, score_backlog(), and changed the i limit on the scoring while loop to twice the number of points to score (allowing for 50% of the scoring attempts to fail).
    Given that the add-on advertises, on the Anki site, to work with reviews
    on any platform, I think there should be an automatic check after a
    profile is loaded and synced to see if reviews have been completed on
    another device. Then the user should be prompted to sync those reviews
    to Habitica. 
    
    The current method requires that the user initiate the database check
    manually, which is kind of a pain for those who use desktop Anki less
    than alternatives.
    
    I'll implement that next and make it easy to disable with a boolean
    variable in case it's undesireable for some reason.

--- AnkiHabitica_v2.0/ankihabitica.py
+++ AnkiHabitica_v2.0/ankihabitica.py
@@ -605,16 +605,22 @@ def score_backlog(silent=False):
         #OK, now we can score some points...
         p = 0 #point counter
         i = 0 #limit tries to 25 to prevent endless loop
-        while i < 25 and ah.config[ah.settings.profile]['score'] >= ah.settings.sched and ah.settings.internet:
+        numScores = ah.config[ah.settings.profile]['score'] // ah.settings.sched
+        ah.log.debug("%s points to score" % numScores)
+        progressLabel = "Scoring %s point%s to Habitica" % (numScores, "" if numScores == 0 else "s")
+        mw.progress.start(max = numScores, label = progressLabel)
+        while i <= 2*numScores and ah.config[ah.settings.profile]['score'] >= ah.settings.sched and ah.settings.internet:
             try:
                 ah.habitica.silent_earn_points("Anki Points")
                 ah.config[ah.settings.profile]['score'] -= ah.settings.sched
                 i += 1
                 p += 1
+                mw.progress.update()
             except:
                 i += 1
-        if not silent: utils.showInfo("%s points scored on Habitica" % p)
-        ah.log.error("%s points scored on Habitica" % p)
+        mw.progress.finish()
+        if not silent: utils.showInfo("%s point%s scored on Habitica" % (p, "" if p == 1 else "s"))
+        ah.log.info("%s point%s scored on Habitica" % (p, "" if p == 1 else "s"))
 #        if ah.settings.debug: utils.showInfo("New scorecount: %s" % ah.habitica.hnote['Anki Points']['scorecount'])
         ah.log.info("New scorecount: %s" % ah.habitica.hnote['Anki Points']['scorecount'])
         save_stats(None, None)
